f2a3f4f0fc49f67020700e65095975a53833894d
saazon <saazonpokhrel@gmail.com>
2023-09-11 16:39:53 +0545

    changed

---
+++ area of rec.py
@@ -0,0 +1,7 @@
+def calculate_area_of_rectangle(length,width):
+    area = length * width
+    return area
+length = 5
+width = 3 
+result = calculate_area_of_rectangle (length,width)
+print ("area of reectangle is:" , result)
